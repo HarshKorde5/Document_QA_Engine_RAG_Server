7710b2474215984f8dacc5f6f597c8ddd5fab5fc
HarshKorde5 <harshkorde05@gmail.com>
2025-09-28 18:10:51 +0530

    minor bugs fix and migrations

--- rag_engine/urls.py
+++ rag_engine/urls.py
@@ -1,6 +1,6 @@
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 # Import settings and static for serving uploaded media files
 from django.conf import settings 
 from django.conf.urls.static import static
@@ -11,3 +11,7 @@ urlpatterns = [
     # Map all URLs starting with 'documents/' to the documents app
     path('', include('documents.urls')), 
 ]
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
